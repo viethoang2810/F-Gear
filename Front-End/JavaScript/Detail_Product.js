84f12ab4c1a886217e921fe428e29da71f02fc1a
viethoang2810 <anhdung19022001@email.com>
2022-09-22 13:39:58 +0700

    Update Front-End for Products page

--- Front-End/JavaScript/Detail_Product.js
+++ Front-End/JavaScript/Detail_Product.js
@@ -1,40 +1,4 @@
-//Handle menu-dropdown on header
-var checkClick = false; //set state for clicking which is use to handle display menu
 
-const handleDropdownMenu = (checkClick) => {
-  let menuBtn = document.querySelector(".btn-menu");
-  let menuDropDown = document.querySelector(".navigator-wrapper");
-  menuBtn.addEventListener("click", () => {
-    checkClick = !checkClick;
-
-
-    if (checkClick) {
-      menuDropDown.style.display = "block";
-    } else {
-      menuDropDown.style.display = "none";
-    }
-  });
-};
-
-handleDropdownMenu(checkClick);
-// Handle menu of categories on sidebar 
-
-const handleDropdownSidebar = (checkClick) =>{
-    let menuBtn = document.querySelector(".navigator-btn_menu");
-    let menuDropDown = document.querySelector('.btn-navigator-list');
-
-    menuBtn.addEventListener('click', () =>{
-      checkClick = !checkClick;
-
-
-    if (checkClick) {
-      menuDropDown.style.display = "block";
-    } else {
-      menuDropDown.style.display = "none";
-    }
-    })
-}
-handleDropdownSidebar(checkClick);
 
 // Handle change images of product 
 
@@ -65,3 +29,4 @@ const handleRemoveClass = (index) =>{
         }
     }
 }
+
