639b0e7ed1a8afb1c19d0c3ec31dc3a00def348f
viethoang2810 <anhdung19022001@email.com>
2022-09-23 16:17:23 +0700

    Update Buying History of user

--- Front-End/JavaScript/CommonFeatures.js
+++ Front-End/JavaScript/CommonFeatures.js
@@ -23,27 +23,32 @@ const handleDropdownSidebar = (checkClick) =>{
     let menuBtn = document.querySelector(".navigator-btn_menu");
     let menuDropDown = document.querySelector('.btn-navigator-list');
 
-    menuBtn.addEventListener('click', () =>{
-      checkClick = !checkClick;
-
-
-    if (checkClick) {
-      menuDropDown.style.display = "block";
-    } else {
-      menuDropDown.style.display = "none";
+    if(menuBtn){
+      menuBtn.addEventListener('click', () =>{
+        checkClick = !checkClick;
+  
+  
+      if (checkClick) {
+        menuDropDown.style.display = "block";
+      } else {
+        menuDropDown.style.display = "none";
+      }
+      })
     }
-    })
 }
 handleDropdownSidebar(checkClick);
 //Handle redirect page by JS 
 var paymentRedirectPage = document.querySelector('.payment');
 
-paymentRedirectPage.onclick = (e) =>{
-  window.location.replace("http://127.0.0.1:5500/Front-End/Payment_Guildine.html");
+if(paymentRedirectPage){
+  paymentRedirectPage.onclick = (e) =>{
+    window.location.replace("http://127.0.0.1:5500/Front-End/Payment_Guildine.html");
+  }
 }
 // Handle redirect user page 
 var userRedirect = document.querySelector('.navigator-user-account');
 
 userRedirect.onclick = () =>{
+  console.log(123);
   window.location.replace('http://127.0.0.1:5500/Front-End/User_Profile.html');
 }
