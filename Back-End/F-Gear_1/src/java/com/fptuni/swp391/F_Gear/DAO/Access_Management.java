7417ee6cbf3d1c1349246efccabab46a47106477
dunghq21102001 <dunghqse151397@fpt.edu.vn>
2022-10-08 07:55:16 +0700

    update file at adminpage

--- Back-End/F-Gear_1/src/java/com/fptuni/swp391/F_Gear/DAO/Access_Management.java
+++ Back-End/F-Gear_1/src/java/com/fptuni/swp391/F_Gear/DAO/Access_Management.java
@@ -5,6 +5,8 @@
  */
 package com.fptuni.swp391.F_Gear.DAO;
 
+import com.fptuni.swp391.F_Gear.DTO.Chart;
+import com.fptuni.swp391.F_Gear.DTO.Orders;
 import com.fptuni.swp391.F_Gear.DTO.Users;
 import com.fptuni.swp391.F_Gear.Utils.Constants;
 import com.fptuni.swp391.F_Gear.Utils.DBUtils;
@@ -18,11 +20,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 //import org.apache.tomcat.util.net.Constants;
-
 
 /**
  *
@@ -43,7 +48,7 @@ public class Access_Management {
             user.setUserName(rs.getString("UserName"));
             user.setPassword(rs.getString("Password"));
             user.setFullName(rs.getString("FullName"));
-            user.setPhoneNumber(rs.getString("PhoneNumber"));
+            user.setPhoneNumber(rs.getInt("PhoneNumber"));
             user.setAvatar(rs.getString("Avatar"));
             user.setGender(rs.getString("Gender"));
             user.setRoleName(rs.getString("RoleName"));
@@ -73,7 +78,7 @@ public class Access_Management {
         PreparedStatement stm = con.prepareStatement("insert into Users (UserName, Password, PhoneNumber) values(?,?,?)");
         stm.setString(1, u.getUserName());
         stm.setString(2, u.getPassword());
-        stm.setString(3, u.getPhoneNumber());
+        stm.setInt(3, u.getPhoneNumber());
         int count = stm.executeUpdate();
         if (count == 0) {
             result = false;
@@ -137,5 +142,381 @@ public class Access_Management {
         con.close();
         return result;
     }
-    
+
+    //select monthly sales -DungHQ
+    public List<Chart> selectAllTotalEachMonth() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select MONTH(o.OrderDate) as [month], sum(od.Total) as [total each month]\n"
+                + "from Orders o inner join OrderDetail od on o.OrderID = od.OrderID group by MONTH(o.OrderDate)";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setMonth(rs.getInt("month"));
+                c.setTotal(rs.getInt("total each month"));
+
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //select total product each month -DungHQ
+    public List<Chart> selectAllQuantityEachMonth() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select MONTH(o.OrderDate) as [month], sum(od.Quantity) as [quantity each month]\n"
+                + "from Orders o inner join OrderDetail od on o.OrderID = od.OrderID group by MONTH(o.OrderDate)";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setMonth(rs.getInt("month"));
+                c.setQuantity(rs.getInt("quantity each month"));
+
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+//    Top product of all-time bestsellers -DungHQ
+    public List<Chart> selectTop12SellingProductAllTime() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity] \n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID  \n"
+                + "group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    public String truncateString(String str, int num) {
+        if (str.length() > num) {
+            str.substring(0, num);
+        }
+        return str;
+    }
+
+    //    Top selling products month 1 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth1() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '1' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 2 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth2() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '2' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 3 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth3() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '3' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 4 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth4() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '4' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 5 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth5() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '5' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 6 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth6() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '6' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    //    Top selling products month 7 -DUngHQ
+
+    public List<Chart> selectTopSellingProductMonth7() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '7' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 8 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth8() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '8' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 9 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth9() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '9' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 10 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth10() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '10' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    //    Top selling products month 11 -DUngHQ
+    public List<Chart> selectTopSellingProductMonth11() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '11' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    //    Top selling products month 12 -DUngHQ
+
+    public List<Chart> selectTopSellingProductMonth12() {
+        List<Chart> list = new ArrayList<>();
+        String query = "select  p.ProName, sum(od.Quantity) as [total quantity], sum(od.Total) as [total sale]\n"
+                + "from OrderDetail od , dbo.Product p, Orders o \n"
+                + "where od.ProID = p.ProID and od.OrderID = o.OrderID \n"
+                + "and month(o.OrderDate) = '12' group by p.ProName order by [total quantity] desc";
+        try {
+            Connection con = DBUtils.getConnection();
+            PreparedStatement stm = con.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Chart c = new Chart();
+                c.setProName(rs.getString("ProName"));
+                c.setQuantity(rs.getInt("total quantity"));
+                c.setTotal(rs.getLong("total sale"));
+                list.add(c);
+            }
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Access_Management.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
 }
